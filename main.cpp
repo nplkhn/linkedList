1b1b7e4ccc1036ea8d13fdd589ad3dd05446f093
Nikita Plakhin <nplkhn1@yandex.ru>
2019-12-31 00:15:20 +0300

    version 1.1

--- main.cpp
+++ main.cpp
@@ -6,9 +6,14 @@ using namespace std;
 struct map{
     string key;
     int value;
+    map* next = nullptr;
     map(){
         key = "";
         value = 0;
+    }
+    map(string key, int value){
+        map::key = key;
+        map::value = value;
     }
 };
 
@@ -19,37 +24,36 @@ public:
     void add(string key, int value);
     void list_size();
     void show();
-    void update(string key, int value);
-    map find(string key, int value);
+    void update(string aim_key, int aim_value, string new_key, int new_value);
+    void del(string key, int value);
 private:
-    map value;
+    map* find(string key, int value);
+    map* head = nullptr;
     int size;
-    n_linkedList* next = nullptr;
+
+
 };
 
 n_linkedList::n_linkedList() {
     this->size = 1;
-    this->value.key = "";
-    this->value.value = 0;
+    map* current = new map;
+    this->head = current;
 }
 
 n_linkedList::n_linkedList(string key, int value) {
     this->size = 1;
-    this->value.key = key;
-    this->value.value = value;
+    map* current = new map(key, value);
+    this->head = current;
 }
 
 void n_linkedList::add(string key, int value) {
     this->size++;
-    n_linkedList* current = this;
+    map* current = this->head;
     while (current->next){
         current = current->next;
     }
-    n_linkedList* new_member = new n_linkedList;
+    map* new_member = new map(key, value);
     current->next = new_member;
-    current = current->next;
-    current->value.key = key;
-    current->value.value = value;
 }
 
 void n_linkedList::list_size(){
@@ -57,40 +61,40 @@ void n_linkedList::list_size(){
 }
 
 void n_linkedList::show() {
-    n_linkedList* current = this;
+    map* current = this->head;
     while (current){
-        cout << current->value.key << " " << current->value.value << endl;
+        cout << current->key << ": " << current->value << endl;
         current = current->next;
     }
 }
 
-void n_linkedList::update(string key, int value) {
-    n_linkedList* current = this;
-    while (current){
-        if (current->value.key == key){
-            current->value.value=value;
-            break;
-        }
-        else if (!current->next){
-            cout << "No member" << endl;
-            break;
-        }
-        else
-            current = current->next;
-    }
+void n_linkedList::update(string aim_key, int aim_value, string new_key, int new_value) {
+    map* aim = this->find(aim_key, aim_value);
+    aim->key = new_key;
+    aim->value = new_value;
 }
 
-map n_linkedList::find(string key, int value) {
-    n_linkedList* current = this;
+map* n_linkedList::find(string key, int value) {
+    map* current = this->head;
     while (current) {
-        if (current->value.key == key and current->value.value == value) {
-            return current->value;
+        if (current->key == key and current->value == value){
+            return current;
         }
         current = current->next;
+    }
+}
+
+void n_linkedList::del(string key, int value) {
+    map* current = this->head;
+    while(current){
+        if (current->next->key == key and current->next->value == value){
+            current = current->next->next;
+        }
     }
 }
 
 int main() {
+    cout << "Testing add method" << endl;
     n_linkedList list("Nikita", 1);
     list.add("Karina", 1);
     list.add("Maria", 1);
@@ -100,13 +104,16 @@ int main() {
     list.add("Anastasia", 4);
     list.list_size();
     list.show();
-
-    list.update("Karina", 10);
+    cout << "\nTesting update method" << endl;
+    list.update("Karina", 1, "Karina", 10);
     list.show();
-    list.update("Viola", 1);
-
-    map test;
-    test = list.find("Karina", 10);
-    cout << test.key << " " << test.value << endl;
+    list.update("Viola", 1, "Karina", 10);
+    cout << "\nTesting del method" << endl;
+    list.del("Nikita", 1);
+    list.show();
+    cout << endl;
+    list.del("Alexandra", 2);
+    list.show();
+    cout << "\nTesting find method" << endl;
     return 0;
 }
